6e50650ae819c206375902cf718c283cb058a295
unknown <shaanmeet1@gmail.com>
2023-08-14 13:48:13 -0600

    PasswordSystem

--- src/level1/projects/PasswordSystem.java
+++ src/level1/projects/PasswordSystem.java
@@ -20,6 +20,7 @@ public class PasswordSystem {
 
         char[] a = str.toCharArray();
 
+
         for (int i = 0; i < a.length; i++) {
 
             if (a[i] >= 65 && a[i] <= 90) {
@@ -37,9 +38,7 @@ public class PasswordSystem {
             if ((a[i] >= 33 && a[i] <= 47) || (a[i] >= 58 && a[i] <= 64) || (a[i] >= 91 && a[i] <= 96) || (a[i] >= 124 && a[i] <= 126)) {
                 specialCharchter = true;
             }
-
         }
-
         if (uppercaseCondition && lowercaseCondition && specialCharchter && numericalValue) {
             return true;
         } else {
@@ -48,25 +47,45 @@ public class PasswordSystem {
     }
 
     public static String PasswordCreation() {
-        char[] a = {'a','b','c','d','e','d','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        char[] b;
-        for (int i=0;i < a.length;i++){
+        char[] a = {'a', 'b', 'c', 'd', 'e', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] b = new char[26];
+        for (int i = 0; i < a.length; i++) {
             b = new char[]{(char) (a[i] + 32)};
         }
-        char[] c = {0,1,2,3,4,5,6,7,8,9};
-        char[] d = {(char)(33),(char)(34),(char)(35),(char)(36),(char)(37),(char)(38),(char)(39),(char)(40),(char)(41),(char)(42),(char)(43),(char)(44),(char)(45),(char)(46),(char)(47),(char)(58),(char)(59),(char)(60),(char)(61),(char)(62),(char)(63),(char)(64),(char)(91),(char)(92),(char)(93),(char)(94),(char)(95),(char)(96),(char)(124),(char)(125),(char)(126)};
-        String str2 = "!@#$%^&*()-=[]" + (char)(92) + ";'/.,<>?:{}|+_";
+        int[] c = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //char[] d = {(char) (33), (char) (34), (char) (35), (char) (36), (char) (37), (char) (38), (char) (39), (char) (40), (char) (41), (char) (42), (char) (43), (char) (44), (char) (45), (char) (46), (char) (47), (char) (58), (char) (59), (char) (60), (char) (61), (char) (62), (char) (63), (char) (64), (char) (91), (char) (92), (char) (93), (char) (94), (char) (95), (char) (96), (char) (124), (char) (125), (char) (126)};
+        String str2 = "!@#$%^&*()-=[]" + (char) (92) + ";'/.,<>?:{}|+_";
         char[] e = str2.toCharArray();
+
+        String str = "";
+        for (int i = 0; i < (int) (Math.random() * a.length - 1); i++) {
+            str = str + a[(int) (Math.random() * (a.length - 1))];
+        }
+        for (int i = 0; i < (int) (Math.random() * b.length - 1); i++) {
+            str = str + b[(int) (Math.random() * (b.length - 1))];
+        }
+
+        for (int i = 0; i < (int) (Math.random() * c.length - 1); i++) {
+            str = str + c[(int) (Math.random() * (c.length - 1))];
+        }
+
+        for (int i = 0; i < (int) (Math.random() * e.length - 1); i++) {
+            str = str + e[(int) (Math.random() * (e.length - 1))];
+        }
+        return str.toString();
+    }
+
+    public static void passwordCheck() {
         boolean conditions = false;
         String str = "";
         while (!conditions) {
-            conditions = conditionCheck(str);
+            str = PasswordCreation();
+            conditions = conditionCheck(str.toString());
             if (conditions) {
                 break;
-            } else {
             }
         }
-        return str;
+
     }
 
     public static void main(String[] args) {
