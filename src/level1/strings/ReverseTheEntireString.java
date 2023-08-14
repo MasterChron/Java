0a8b70e2835c658e80e7332e80dd4182f86e9920
unknown <shaanmeet1@gmail.com>
2023-08-14 13:50:32 -0600

    C:\Users\user\IdeaProjects\JavaLearning\src\level1\strings\ReverseTheEntireString.java

--- src/level1/strings/ReverseTheEntireString.java
+++ src/level1/strings/ReverseTheEntireString.java
@@ -1,2 +1,26 @@
-package level1.strings;public class ReverseTheEntireString {
+package level1.strings;
+
+public class ReverseTheEntireString {
+
+    //Reverse the String - III
+    //You have the reverse the entire string. Example is given below
+    //
+    //Ex - “Java is cool”
+    //Reversed - “looc si avaJ”
+
+    public static void reverseString(String str){
+        char[] a = str.toCharArray();
+        StringBuilder str2 = new StringBuilder();
+        String str3 = "";
+        for(int i=a.length - 1; i >= 0; i--){
+            str2.append(a[i]);
+            str3 = str3 + a[i];
+        }
+        System.out.println("Reversed String: " + str2.toString());
+        System.out.println(str3);
+    }
+
+    public static void main(String[] args) {
+        reverseString("This is a String.");
+    }
 }
