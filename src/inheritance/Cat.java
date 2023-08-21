bb120c6fa6b205211537d8d9557b0a2f5b5fa21c
akshay-03 <agarwal3akshay@gmail.com>
2023-08-21 08:09:45 +0530

    inheritance

---
+++ src/inheritance/Cat.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Cat extends Feline {
+
+    void walk() {
+        System.out.println("Cat walks");
+    }
+
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.walk();
+    }
+}
